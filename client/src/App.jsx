18f1425f606bee0ee9aeda3f41086aa331de2fab
DSYS <dinodimanjith404@gmail.com>
2025-06-19 14:48:31 +0530

    route path added for each pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import Result from './pages/Result'
+import BuyCredit from './pages/BuyCredit'
+import NavBar from './components/NavBar'
 
 const App = () => {
   return (
     <div>
-      Working
+      <NavBar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/result' element={<Result/>}/>
+        <Route path='/buy' element={<BuyCredit/>}/>
+      </Routes>
     </div>
   )
 }
