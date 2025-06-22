f9c70193b5fd45d2c77012cfcf4c38984a1e303d
DSYS <dinodimanjith404@gmail.com>
2025-06-22 18:54:25 +0530

    Clerk Authentication added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,17 +1,29 @@
 import React from 'react'
 import { assets } from '../assets/assets'
 import {Link} from 'react-router-dom'
+import {useClerk, UserButton, useUser} from '@clerk/clerk-react'
 
 const NavBar = () => {
+
+  const {openSignIn}=useClerk()
+  const {isSignedIn,user}=useUser()
+
   return (
     <div className='flex items-center justify-between mx-4 py-3 lg:mx-44'>
       <Link to={'/'}>
         <img src={assets.logo} alt="Logo" className="h-8 sm:h-10" />
       </Link>
-      <button className='bg-zinc-800 text-white flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 text-sm sm:text-base rounded-full'>
+      {
+        isSignedIn 
+        ?<div>
+          <UserButton/>
+        </div>
+        :<button onClick={()=>openSignIn({})} className='bg-zinc-800 text-white flex items-center gap-2 px-4 py-2 sm:px-6 sm:py-3 text-sm sm:text-base rounded-full'>
         <span>Get Started</span>
         <img className='w-4 sm:w-5' src={assets.arrow_icon} alt="Arrow" />
       </button>
+      }
+      
     </div>
   )
 }
