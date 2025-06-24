058098bcc5a5090d2774aa64883d42638089b44f
DSYS <dinodimanjith404@gmail.com>
2025-06-24 08:10:38 +0530

    get userCredits function implemented

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -57,4 +57,19 @@ const clerkWebhooks = async (req, res) => {
     }
 }
 
-export { clerkWebhooks }
+//API controller function to get user avaible credits data
+const userCredits=async (req,res)=>{
+    try {
+        const {clerkId}=req.body
+
+        const userData=await userModel.findOne({clerkId})
+
+        res.json({ success: true, credits:userData.creditBalance })
+
+    } catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message })
+    }
+}
+
+export { clerkWebhooks,userCredits }
